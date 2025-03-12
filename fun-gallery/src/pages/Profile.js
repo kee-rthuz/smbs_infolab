7be3cee20d22f23c9b8332affa79cc5fd51e1c2d
kee-rthuz <101699756+kee-rthuz@users.noreply.github.com>
2025-03-12 21:30:46 +0530

    Update Profile.js

--- fun-gallery/src/pages/Profile.js
+++ fun-gallery/src/pages/Profile.js
@@ -44,11 +44,14 @@ const Profile = () => {
   const handleProfilePictureChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      const preview = URL.createObjectURL(file);
-      setUser((prevUser) => ({
-        ...prevUser,
-        profilePicture: preview,
-      }));
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setUser((prevUser) => ({
+          ...prevUser,
+          profilePicture: reader.result,
+        }));
+      };
+      reader.readAsDataURL(file);
     }
   };
 
